fc4bb18660dfba1c959fb3d4adebedc6d0a823ba
Janio Carrilho <janiolcarrilho@gmail.com>
2024-06-26 13:47:33 -0300

    Atualização crud Registros

--- app/src/controller/RegistroController.ts
+++ app/src/controller/RegistroController.ts
@@ -1,7 +1,14 @@
 import { Registro } from "../model/Registro"
+
+const model = new Registro()
 export let getRegistroTreino = async function (request, reply){
     try {
-        
+        const registro = model.getRegistroTreino(request.query.id)
+        return {
+            data: {
+                registro: registro
+            }
+        }
     } catch (error) {
         reply.status(500)
         reply.send(error.message)
@@ -9,7 +16,11 @@ export let getRegistroTreino = async function (request, reply){
 }
 export let createRegistroTreino = async function (request, reply){
     try {
-        
+        const registro = await model.createRegistroTreino(request)
+        reply.send({
+            message: "registro realizado com sucesso",
+            data: registro
+        })
     } catch (error) {
         reply.status(500)
         reply.send(error.message)
@@ -18,7 +29,11 @@ export let createRegistroTreino = async function (request, reply){
 }
 export let createRegistroExercicio = async function (request, reply){
     try {
-        
+        const registro = await model.createRegistroExercicio(request)
+        reply.send({
+            message: "registro realizado com sucesso",
+            data: registro
+        })
     } catch (error) {
         reply.status(500)
         reply.send(error.message)
