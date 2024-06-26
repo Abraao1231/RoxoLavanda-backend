190578bba84a9526f8b1fdbde3fbfc908a89d2c2
Janio Carrilho <janiolcarrilho@gmail.com>
2024-06-25 21:15:52 -0300

    add-model-registro

--- app/src/model/Registro.ts
+++ app/src/model/Registro.ts
@@ -1,6 +1,36 @@
+import {prisma} from '../lib/prisma'
+import {z} from 'zod'
+import dayjs from 'dayjs'
+
+
 export class Registro {
     constructor(){}
     async createRegistroTreino(request: Request){
-        
+        const today = dayjs().startOf('day').toDate();
+
+        const zodBodyRegistro = z.object({
+            userId: z.string(),
+            treinoId: z.string(),
+            tempo: z.number()
+        })
+
+        const {userId, treinoId, tempo} = zodBodyRegistro.parse(request.body)
+        const registro = await prisma.registroTreinoRealiza.create({
+            data: {
+                user: {
+                    connect: {
+                        id: userId
+                    }
+                },
+                treino: {
+                    connect: {
+                        id: treinoId
+                    }
+                },
+                tempo: tempo,
+                data: today
+            }
+        })
+        return registro
     }
 }
